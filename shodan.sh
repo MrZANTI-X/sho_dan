f9a8cc2b96d194534e89b2f5119fecf80e8f4256
zanti1 <81058479+zanti1@users.noreply.github.com>
2021-05-02 07:39:27 +0530

    Add files via upload

---
+++ shodan.sh
@@ -0,0 +1,77 @@
+A1="\e[0;31m" #Red
+A2="\e[0;33m" # Yellow
+A3="\e[0;34m" # Blue
+A4="\e[0;32m" #green
+bash banner.sh
+line() {
+echo -en $A2 '
+[  0 ] clear-Desplay
+[  1 ] shodan-install
+[  2 ] run-shodan
+[  3 ] shodan-version
+[  4 ] open-shodan
+[  5 ] shodan-api
+[  6 ] count-webcam
+[  7 ] count-cctv
+[  8 ] shodan-host
+[  9 ] myip
+[ 10 ] shodan-info
+[  A ] about
+'
+}
+line
+read -p "enter : " data
+case $data in
+0)
+  echo "clear-Desplay "
+  clear
+;;
+1)
+  echo "shodan-install "
+  bash sho.install.sh
+  clear
+;;
+2)
+  echo "ren-shodan"
+  bash sho-dan.sh
+;;
+3)
+  echo "shodan-version "
+  shodan version
+;;
+4)
+  echo "open-shodan "
+  termux-open-url https://www.shodan.io/
+;;
+5)
+  echo "shodan-api"
+  read -p "En : " hart
+  shodan init $hart
+  clear
+;;
+6)
+  echo "count-webcam"
+  shodan count 'webcamxp'
+;;
+7)
+  echo "count-cctv"
+  shodan count cctv
+;;
+8)
+  echo "shodan-host"
+  read -p "enter ip : " SSD
+  shodan host $SSD
+;;
+9)
+  echo "myip "
+  shodan myip
+;;
+10)
+  echo "shodan-info"
+  shodan info 
+;;
+A*)
+  echo "about "
+  termux-open-url https://youtu.be/CxaPEDXVH7w
+;;
+esac
