fe877bd02145ec11d245d15f2a0b4f7d95fd23af
zanti1 <81058479+zanti1@users.noreply.github.com>
2021-05-07 06:23:43 +0530

    Add files via upload

---
+++ scan-ip.py
@@ -0,0 +1,54 @@
+#hello user 
+#mode by Mr. zanti -X
+import requests, re , colorama
+colorama.init()
+print("hello user")
+print("""
+1 us   11 TR   
+2 JP   12 AT
+3 IT   13 CH
+4 KR   14 ES
+5 FR   15 CA
+6 DR   16 SE
+7 TW   17 IL
+8 RU   18 PL
+9 GB   19 IR
+10 CZ  20 NO
+""")
+try:
+    print()
+    countries = ["US", "JP", "IT", "KR", "FR", "DE", "TW", "RU", "GB", "NL",
+                 "CZ", "TR", "AT", "CH", "ES", "CA", "SE", "IL", "PL", "IR",
+                 "NO", "RO", "IN", "VN", "BE", "BR", "BG", "ID", "DK", "AR",
+                 "MX", "FI", "CN", "CL", "ZA", "SK", "HU", "IE", "EG", "TH",
+                 "UA", "RS", "HK", "GR", "PT", "LV", "SG", "IS", "MY", "CO",
+                 "TN", "EE", "DO", "SI", "EC", "LT", "PS", "NZ", "BD", "PA",
+                 "MD", "NI", "MT", "IT", "SA", "HR", "CY", "PK", "AE", "KZ",
+                 "KW", "VE", "GE", "ME", "SV", "LU", "CW", "PR", "CR", "BY",
+                 "AL", "LI", "BA", "PY", "PH", "FO", "GT", "NP", "PE", "UY",
+                 "-"]
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux i686; rv:68.0) Gecko/20100101 Firefox/68.0"}
+
+    num = int(input("OPTIONS : "))
+    if num not in range(1, 91+1):
+        raise IndexError
+
+    country = countries[num-1]
+    res = requests.get(
+        f"https://www.insecam.org/en/bycountry/{country}", headers=headers
+    )
+    last_page = re.findall(r'pagenavigator\("\?page=", (\d+)', res.text)[0]
+
+    for page in range(int(last_page)):
+        res = requests.get(
+            f"https://www.insecam.org/en/bycountry/{country}/?page={page}",
+            headers=headers
+        )
+        find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)
+        for ip in find_ip:
+            print("serach = ", ip)
+except:
+    pass
+finally:
+    exit()
+
